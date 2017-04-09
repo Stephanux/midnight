68796278b0df15701a0418288363eb6039656ab2
st3ffane <steph.ponteins@gmail.com>
2017-04-09 20:42:49 +0200

    hbs init

--- hbs.init.js
+++ hbs.init.js
@@ -8,11 +8,20 @@ let hbs = require("hbs");
 //configuration HBS pour le site
 hbs.registerPartials(__dirname + '/views/partials');
 
-hbs.registerHelper('eachExcept',function(obj, except, options){
+hbs.registerHelper('eachExcept',function(){
     //parcours uniqument les methodes http, ie keys!="childRoutes"
     var ret = "";
+    var except = [...arguments];
+    var options = except.pop();//options, le dernier argument 
+    var obj = except.shift();//l'objet cible
+    
+
+   
     for (var key of Object.keys(obj)){
-        if(key != except) ret+=options.fn({key:key, value:obj[key]});
+        if(except.indexOf(key)==-1) ret+=options.fn({key:key, value:obj[key]});
+        
     }
+    
+    
     return ret;
 });
